2efafa85f35c00eeda552eae2e484f78178dd5c6
Aaron <relojvale25@gmail.com>
2022-10-23 16:13:19 -0400

    Pagina Principal + JS

--- client/display.js
+++ client/display.js
@@ -1,3 +1,4 @@
+console.log("Display cargada");
 let products = {
     data:[
         {
@@ -9,7 +10,7 @@ let products = {
             imagen: "https://www.gannett-cdn.com/presto/2021/11/19/USAT/cd15a425-e751-46a8-98c2-86993fcf9dce-croc.png?width=660&height=372&fit=crop&format=pjpg&auto=webp"
         },
         {
-            id_producto: "10521",
+            id_producto: "10522",
             nombre: "Crocodile Lego set",
             descripcion: "Un armable de cocodrilo",
             categoria: "Creator",
@@ -17,7 +18,7 @@ let products = {
             imagen: "https://www.gannett-cdn.com/presto/2021/11/19/USAT/cd15a425-e751-46a8-98c2-86993fcf9dce-croc.png?width=660&height=372&fit=crop&format=pjpg&auto=webp"
         },
         {
-            id_producto: "10521",
+            id_producto: "10523",
             nombre: "Crocodile Lego set",
             descripcion: "Un armable de cocodrilo",
             categoria: "Creator",
@@ -27,12 +28,69 @@ let products = {
     ]
 }
 
+let items_selected = {}
+
 let frag = document.createDocumentFragment()
-for (let i of products.data) {
-    let prevPrice = i.precio * 1.1
+let products_list = document.getElementById("products")
+
+let catalog = document.getElementById("catalog")
+let sidemenu = document.getElementById("sidemenu")
+
+let update_items_selected = () => {
+    let list  =document.getElementById("selected_products")
+    while (list.firstChild) {
+        list.removeChild(list.lastChild);
+      }
+    let items_frag = document.createDocumentFragment()
+    for (const key in items_selected) {
+        if (Object.hasOwnProperty.call(items_selected, key)) {
+            let item = document.createElement("div")
+            item.classList.add("selected_product")
+            item.id = key
+            item.innerHTML = `
+            <h5>${items_selected[key][0]}</h5>
+            <p>${items_selected[key][1]}</p>
+            `
+            items_frag.appendChild(item)
+        }
+    }
+    list.appendChild(items_frag)
+}
+
+let agregar_item = (id) => {
+    // console.log("Agregaste:" + id);
+    // console.log(items_selected);
+    if (Object.hasOwnProperty.call(items_selected, id)) {
+        items_selected[id][1] ++;
+    } else {
+        let nombre_aux = ""
+        console.log(typeof(id));
+        nombre_aux = (products.data.find((item) => {
+            return item.id_producto == parseInt(id)
+        })).nombre
+        items_selected[id] = [nombre_aux, 1]
+    }
+}
+
+let retirar_item = (id) => {
+    console.log("Eliminaste:" + id);
+    if (Object.hasOwnProperty.call(items_selected, id)) {
+        if (items_selected[id][1] > 0) {
+            items_selected[id][1] --;
+        }
+        if (items_selected[id][1] == 0) {
+            delete items_selected[id];
+        }
+    }
+}
+
+
+for (let product of products.data) {
+    let prevPrice = product.precio * 1.1
     // Card
     let card = document.createElement("div")
     card.classList.add("card")
+    card.id = product.id_producto
     card.innerHTML = `
         <div class="card_image">
             <div class="card_fav">
@@ -43,37 +101,43 @@ for (let i of products.data) {
                 </button>
             </div>
             <div class="card_image-container">
-                <img src="${i.imagen}" alt="">
+                <img src="${product.imagen}" alt="">
             </div>
         </div>
         <div class="card_details">
-            <p class="card_code subtitle">Vendor code: ${i.id_producto}</p>
-            <h3 class="card_title">${i.nombre}</h3>
-            <p class="card_description">${i.descripcion}</p>
+            <p class="card_code subtitle">Vendor code: ${product.id_producto}</p>
+            <h3 class="card_title">${product.nombre}</h3>
+            <p class="card_description">${product.descripcion}</p>
         </div>
         <div class="card_options">
             <div class="price_details">
                 <p class="subtitle">Precio:</p>
                 <div class="prices">
-                    <h3 class="card_price">${i.precio}$</h3>
+                    <h3 class="card_price">${product.precio}$</h3>
                     <p class="subtitle">${prevPrice}$</p>
                 </div>
             </div>
-            <a href="" class="card_action">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                </svg>
-            </a>
+            <div class="card_btns" id ="${product.id_producto}">
+                <button class="card_action delete_product">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                    </svg>
+                </button>
+                <button class="card_action add_product">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                    </svg>
+                </button>
+            </div>
         </div>
     `
     frag.appendChild(card)
 }
-document.getElementById("products").appendChild(frag)
+products_list.appendChild(frag)
 
-let sidemenu = document.getElementById("sidemenu")
+
 sidemenu.addEventListener("click", (e) => {
-    // e.preventDefault();
-    // console.log(e)
     if (e.target.classList.contains("item-title") || e.target.classList.contains("item-icon")) {
         e.target.parentElement.parentElement.classList.toggle("active")        
     } else if(e.target.classList.contains("item-header")){
@@ -81,6 +145,21 @@ sidemenu.addEventListener("click", (e) => {
     }
 })
 
-// window.onload = () => {
-//     filterProduct("all")
-// }
+catalog.addEventListener("click", (e) => {
+    if (e.target.classList.contains("delete_product")) {
+        // console.log(e.target.parentElement.id);
+        console.log("Retirando");
+        retirar_item(e.target.parentElement.id)
+
+    } else if (e.target.classList.contains("add_product")) {
+        console.log("agregando");
+        agregar_item(e.target.parentElement.id)
+    }
+    update_items_selected()
+})
+
+
+window.onload = () => {
+    // filterProduct("all")
+    update_items_selected()
+}
